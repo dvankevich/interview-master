5d2593955b919f874be5132e64fc5d18713175e8
dvankevich <dvankevich@gmail.com>
2025-04-24 17:13:04 +0300

    Property binding

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -12,6 +12,10 @@ export class TestComponent {
   firstName = 'FirstName';
   lastName = 'LastBame';
 
+  isEnabled: boolean = true;
+  // isEnabled: boolean = false;
+  imageUrl = 'https://picsum.photos/200';
+
   getFullName() {
     return `My name is ${this.firstName} ${this.lastName}`;
   }
